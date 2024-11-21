d60157b7b002635d34f505f34b6e2f7070c8d2e4
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-11-21 10:26:42 +0800

    commit pertanyaan 2

--- Kafe24.java
+++ Kafe24.java
@@ -1,6 +1,6 @@
 public class Kafe24 {
-    public static void Menu() {
-        System.out.println("===== MENU RESTO CAFE =====");
+    public static void main(String[] args) {
+        System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
@@ -8,11 +8,6 @@ public class Kafe24 {
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
-        System.out.println("Silahkan pilih menu yang Anda inginkan.");
-    }
-
-    public static void main(String[] args) {
-        Menu();
-
+        System.out.println("Silahkan pilih menu yang Anda inginkan");
     }
 }
